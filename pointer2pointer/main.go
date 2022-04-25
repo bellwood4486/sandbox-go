1140a15287ea1ef91cec4c5a4d65d4cd3795ce4f
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2022-04-25 23:40:01 +0900

    feat: Add another sample of pointer to pointer

--- pointer2pointer/main.go
+++ pointer2pointer/main.go
@@ -2,12 +2,9 @@ package main
 
 import "fmt"
 
-// see: https://www.geeksforgeeks.org/go-pointer-to-pointer-double-pointer/
-func main() {
+func example1() {
 	var v int = 100
-
 	var pt1 *int = &v
-
 	var pt2 **int = &pt1
 
 	fmt.Println("The Value of Variable v is = ", v)
@@ -21,4 +18,26 @@ func main() {
 	fmt.Println("Value at the address of pt2 is or *pt2 = ", *pt2)
 
 	fmt.Println("*(Value at the address of pt2 is) or **pt2 = ", **pt2)
+}
+
+func example2() {
+	var v int = 100
+	var pt1 *int = &v
+	var pt2 **int = &pt1
+
+	fmt.Println("The value of variable v is = ", v)
+
+	*pt1 = 200
+
+	fmt.Println("Value stored in v after changing pt1 = ", v)
+
+	**pt2 = 300
+
+	fmt.Println("Value stored in v after changing pt2 = ", v)
+}
+
+// see: https://www.geeksforgeeks.org/go-pointer-to-pointer-double-pointer/
+func main() {
+	example1()
+	example2()
 }
